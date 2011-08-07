f5ed61c187fbe3ebf4e2b14b28b2fa901d278e73
Josiah Worcester <josiahw@gmail.com>
2011-08-07 16:30:14 -0600

    Build on i[456]86 systems.

--- musl-headers.build.sh
+++ musl-headers.build.sh
@@ -1,15 +1,21 @@
 #!/bin/sh -e
 VER=0.7.12
+
+if [ "$(printf "%s" "$A" | sed 's/i[3456]86//')" = "" ];then
+    export ARCH=i386
+else
+    export ARCH="$A"
+fi
+
 test -e musl-$VER.tar.gz || wget http://www.etalabs.net/musl/releases/musl-$VER.tar.gz
 rm -rf musl-$VER;tar -xf musl-$VER.tar.gz
 cd musl-$VER
 cat >config.mak <<EOF
-ARCH = $A
 prefix = $S
 exec_prefix = $S
 syslibdir = $S/lib/
 EOF
-ln -sf ../arch/$A/bits include/bits
+ln -sf ../arch/$ARCH/bits include/bits
 sh include/bits/alltypes.h.sh >include/bits/alltypes.h
 for i in include/*.h include/*/*.h;do
     make $S/$i
--- musl.build.sh
+++ musl.build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -e
 VER=0.7.12
+
+if [ "$(printf "%s" "$A" | sed 's/i[3456]86//')" = "" ];then
+    export ARCH=i386
+else
+    export ARCH="$A"
+fi
+
 test -e musl-$VER.tar.gz || wget http://www.etalabs.net/musl/releases/musl-$VER.tar.gz
 rm -rf musl-$VER;tar -xf musl-$VER.tar.gz
 cd musl-$VER
 cat >config.mak <<EOF
-ARCH = $A
 prefix = $S
 exec_prefix = $R
 syslibdir = $S/lib/
